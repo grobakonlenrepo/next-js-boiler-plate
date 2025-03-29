e7fde20043eff6cde12e151021e51c499f9c1537
grobakonlenrepo <grobakonlen@gmail.com>
2025-03-29 11:27:59 +0700

    add -> constants

---
+++ public/constants/index.ts
@@ -0,0 +1,138 @@
+// Navbar
+export const navContent = {
+  logo: "/favicon.ico",
+
+  menu: [
+    {
+      id: "0",
+      menu: "Menu-1",
+      href: "#menu-1",
+    },
+    {
+      id: "1",
+      menu: "Menu-2",
+      href: "#menu-2",
+    },
+    {
+      id: "2",
+      menu: "Menu-3",
+      href: "#menu-3",
+    },
+  ],
+
+  cta: "Contact Us",
+};
+
+// Hero
+export const heroContent = {
+  heading1: "Heading 1",
+  paragraph: "Paragraph",
+  cta: "Cta",
+  productImg: "/images/product.webp",
+};
+
+// Product
+export const productContent = [
+  {
+    id: "0",
+    product1: "/images/product1.webp",
+    price: "10K",
+    cta: "Order Now",
+  },
+  {
+    id: "1",
+    product1: "/images/product2.webp",
+    price: "10K",
+    cta: "Order Now",
+  },
+  {
+    id: "2",
+    product1: "/images/product3.webp",
+    price: "10K",
+    cta: "Order Now",
+  },
+  {
+    id: "3",
+    product1: "/images/product4.webp",
+    price: "10K",
+    cta: "Order Now",
+  },
+  {
+    id: "4",
+    product1: "/images/product5.webp",
+    price: "10K",
+    cta: "Order Now",
+  },
+];
+
+// Testimonials
+export const testiContent = [
+  {
+    id: "0",
+    person1: "/images/person1.webp",
+    name: "Person1",
+    title: "Mahasiswa UMP",
+    text: "Enak cuy, murah lagi, gass cobain",
+  },
+  {
+    id: "1",
+    person1: "/images/person2.webp",
+    name: "Person2",
+    title: "Mahasiswa UMP",
+    text: "Enak cuy, murah lagi, gass cobain",
+  },
+  {
+    id: "2",
+    person1: "/images/person3.webp",
+    name: "Person3",
+    title: "Mahasiswa UMP",
+    text: "Enak cuy, murah lagi, gass cobain",
+  },
+];
+
+// FAQ
+export const faqContent = [
+  {
+    id: "0",
+    number: "1",
+    question: "Question 1",
+    answer: "Answer 1",
+  },
+  {
+    id: "0",
+    number: "2",
+    question: "Question 2",
+    answer: "Answer 2",
+  },
+  {
+    id: "0",
+    number: "3",
+    question: "Question 3",
+    answer: "Answer 3",
+  },
+];
+
+// Footer
+export const footerContent = {
+  logo: "/logo.svg",
+  heading2: "Heading 2",
+  socmed: "Follow sosial media kami",
+
+  socmedIcon: [
+    {
+      id: "0",
+      icon1: "/images/instagram.svg",
+      href: "/instagram.com/example",
+    },
+    {
+      id: "1",
+      icon2: "/images/tiktok.svg",
+      href: "/tiktok.com/example",
+    },
+    {
+      id: "2",
+      icon3: "/images/youtube.svg",
+      href: "/youtube.com/example",
+    },
+  ],
+};
